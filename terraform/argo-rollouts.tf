0b09c60477f4432574482f11e7624a415811cc72
Shipps <sractbr@gmail>
2026-03-19 15:12:50 -0300

    feat: adicionando blueprint do argocd e corrigindo metadados

--- terraform/argo-rollouts.tf
+++ terraform/argo-rollouts.tf
@@ -8,7 +8,7 @@ resource "helm_release" "argo_rollouts" {
   name       = "argo-rollouts"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-rollouts"
-  namespace  = kubernetes_namespace.argo_rollouts.metadata.name
+  namespace  = kubernetes_namespace.argo_rollouts.metadata[0].name
 
   # Adiciona a dependência para garantir que o cluster EKS exista primeiro
   depends_on = [
---
+++ terraform/argocd.tf
@@ -0,0 +1,10 @@
+resource "helm_release" "argocd" {
+  name             = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  create_namespace = true
+  version          = "5.46.7"
+
+  depends_on = [module.eks]
+}
